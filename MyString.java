2539fc698aab97f8c0fd033a48033edca7edc777
Omer Abuddi <shira.abuddi@post.runi.ac.il>
2024-12-10 23:57:19 +0200

    attampt1

--- MyString.java
+++ MyString.java
@@ -7,7 +7,7 @@ public class MyString {
         System.out.println(countChar(hello, 'h'));
         System.out.println(countChar(hello, 'l'));
         System.out.println(countChar(hello, 'z'));
-        System.out.println(spacedString(hello));
+        System.out.println(subsetOf("c o s t", "o c o s t r z a"));
         System.out.println(remove("committee", "meet"));
 
         //// Put your other tests here.
